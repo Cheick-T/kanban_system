d76e75c57a0f4ce5595da286d056bc21dceab89a
Cheick-T <abdallahatyoro@gmail.com>
2020-08-14 13:06:02 +0000

    Rapport app done

--- Rapports/admin.py
+++ Rapports/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from app.models import * 
 from app.admin import DossierAdmin,MouvementAdmin
 
+from django.db.models import Count
+from django.db.models.functions import TruncDay
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Register your models here.
 
@@ -26,6 +30,30 @@ class RapportMouvement(Mouvement):
 class Mouvement1Admin(MouvementAdmin):
     def get_queryset(self, request):
         return self.model.objects.all()
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new subscribers per day
+        chart_data_in = (
+            Mouvement.objects.filter(sens = "in").annotate(date=TruncDay("creation_time"))
+            .values("date")
+            .annotate(y=Count("sens"))
+            .order_by("-date")
+        )
+
+        chart_data_out = (
+            Mouvement.objects.filter(sens = "out").annotate(date=TruncDay("creation_time"))
+            .values("date")
+            .annotate(y=Count("sens"))
+            .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json_in = json.dumps(list(chart_data_in), cls=DjangoJSONEncoder)
+        as_json_out = json.dumps(list(chart_data_out), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data_in": as_json_in, "chart_data_out":as_json_out}
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
 
 
 admin.site.register(DossierOut, DossierOutAdmin)
