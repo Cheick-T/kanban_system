7e3a2584e8ed74fb390ba76dee9843de17c25fa5
bassory99 <bassory99@gmail.com>
2020-09-25 22:11:20 +0000

    Update urls.py

--- kanban/urls.py
+++ kanban/urls.py
@@ -20,9 +20,3 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
          name='password_reset_complete',),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
