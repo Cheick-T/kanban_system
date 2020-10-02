ff8213928a8ff18730ccb94ba1b8d6b59fe4439b
Cheick-T <63298880+Cheick-T@users.noreply.github.com>
2020-10-02 13:09:20 +0000

    Update models.py
    Protect to OneToOne key

--- app/models.py
+++ app/models.py
@@ -48,7 +48,7 @@ class FolderCategory(TimedModel):
 
 class Agent(TimedModel):
     code = models.CharField("Nom d'utilisateur de l'agent",max_length=10, blank=False) #,verbose_name="Code agent")
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     nom = models.CharField(max_length=50, blank=False, verbose_name="NOM")
     prenoms = models.CharField(max_length=50, blank=True, null=True, verbose_name="Prénoms")
     categorie_agent = models.ForeignKey(
