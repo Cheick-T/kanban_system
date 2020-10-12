f53d042866b29a1b093ad953139f3ee90b3c46ef
bassory99 <bassory99@gmail.com>
2020-10-12 10:04:24 +0000

    Update settings.py

--- kanban/settings.py
+++ kanban/settings.py
@@ -94,10 +94,22 @@ WSGI_APPLICATION = 'kanban.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kanban_data',
+        'USER': 'root',
+        'PASSWORD' : 'mysqlarr01',
+        'HOST': 'db',
+        'PORT': 3306,
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
