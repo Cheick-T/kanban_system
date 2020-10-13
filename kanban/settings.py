a821ab82a900a56f8ca08c0789f5b1dd98dfb3e9
bassory99 <bassory99@gmail.com>
2020-10-13 15:03:08 +0000

    Update settings.py

--- kanban/settings.py
+++ kanban/settings.py
@@ -94,24 +94,24 @@ WSGI_APPLICATION = 'kanban.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kanban_data',
-        'USER': 'root',
-        'PASSWORD' : 'T59PNfzNSb',
-        'HOST': 'db',
-        'PORT': 3306,
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'kanban_data',
+#        'USER': 'root',
+#        'PASSWORD' : 'T59PNfzNSb',
+#        'HOST': 'db',
+#        'PORT': 3306,
+#        'OPTIONS': {'charset': 'utf8mb4'},
+#    }
+#}
 
 
 # Password validation
